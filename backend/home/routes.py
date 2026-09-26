7ea9fe4b4ca30876d400b13f4f1a3ab948d842d8
sholonn <kerorushezhang@gmail.com>
2026-09-26 15:03:03 +0900

    DBの取得してくるデータを必要な情報のみに変更

--- backend/home/routes.py
+++ backend/home/routes.py
@@ -10,10 +10,11 @@ def get_listings():
     try:
         result = (
             supabase.table("chatbot")
-            .select("*")
+            .select("chatbot_id,bot_name,price,description,sale_status,users(user_id,username,profile_image_url),category(category_id,category_name)")
             .neq("sale_status",UNLISTED_STATUS)
             .execute()
         )
         return jsonify(result.data)
     except Exception as e:
+        print(e)
         return jsonify({"error":f"商品の取得に失敗しました: {str(e)}"}),500
